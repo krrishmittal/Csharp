846608b7c574b4c9a3f67d765175fa71a49c0195
krrishmittal <mittalkrrish0811@gmail.com>
2026-01-09 15:01:38 +0530

    Number guess game

---
+++ project/guessNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Random r=new Random();
+        int secret=r.Next(1,10);
+        int attempts=0;
+        int guess=0;
+        // Console.Write(secret+" "+guess);
+        while(guess!=secret){
+            Console.WriteLine("Enter a number: ");
+            try{
+                guess=Convert.ToInt32(Console.ReadLine());
+                attempts++;
+                if(guess>secret){
+                    Console.WriteLine("Too High"); 
+                }
+                else if(guess<secret){
+                    Console.WriteLine("Too Low");
+                }
+                else
+                {
+                    Console.WriteLine("Correct! You guessed the number in: "+attempts+" attempts");
+                }
+            }
+            catch{
+                Console.WriteLine(" Invalid input!");
+            }
+        }
+    }
+}
