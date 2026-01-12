d267e3f818bd1c64956a1d8eea5aaa79d3497a2c
krrishmittal <mittalkrrish0811@gmail.com>
2026-01-12 12:53:06 +0530

    day 4 learning c#

--- basics/day3/Program.cs
+++ basics/day3/Program.cs
@@ -78,7 +78,9 @@
 
 
 // Class and object in C#
-// class is the user defined type that contains fields, methods and its members memory are allocated when the object of the class is being created
+// class is the user defined type that contains fields, methods and its members memory are allocated when the object of the class is being created  This approach makes programs more organized, reusable and easier to maintain.
+
+// Object in C# is something you create from a class, which represents a real-world entity and lets you use the data and actions defined in that class. When an object is created, the class is instantiated. All objects share the class’s behavior, but each has its own unique state. A class can have multiple instances.When we declare a reference variable in C# (like Dog tuffy or Dog freedo), no memory for the object is allocated at that time.
 
 // class ClassName{
 //     // Fields
@@ -106,4 +108,60 @@
 //     }
 // }
 
-// Methods in C#
+
+
+// Methods in C# A method is a block of code that performs a specific task. It can take inputs, return results and is defined within classes to make programs modular, readable and reusable.
+
+// static int Add(int a, int b){
+//     return a + b;
+// }
+// Access Modifier: Defines the visibility of the method.
+// Return Type: Specifies the type of value the method returns.
+// Method Name: The name used to call the method.
+// Parameters: A list of inputs the method can take and it is optional
+// Method Body: The block of code that defines what the method does
+
+//method calling can be of two types direct method calling and static method calling
+// direct method is when we invoke the method using the instance of the class
+// class P
+// {
+//     public void print()
+//     {
+//         Console.WriteLine("Direct method calling");
+//     }
+// }
+// class M()
+// {
+//     public static void Main()
+//     {
+//         P obj=new P();
+//         obj.print(); 
+//     }
+// }
+
+// static method we dont need to create the object of the class we can use the method by using the class name directly
+// class P
+// {
+//     public static int print(int value)
+//     {
+//         return value*value;
+//     }
+// }
+// class M()
+// {
+//     public static void Main()
+//     {
+//         int res=P.print(4);
+//         Console.Write(res);
+//     }
+// }
+
+// Method Parameters
+// Parameter       Description                                 Example
+
+// Value           Passes a copy of the argument               void Display(int x)
+
+// Reference       Passes a reference to the argument          void Update(ref int x)
+
+// Output          Parameter Used to return multiple values    void GetValues(out int x)
+
---
+++ basics/day4/Program.cs
@@ -0,0 +1,139 @@
+﻿// properties in C# is a member in C# that allows to read write or compute the value of the private field easily by using the get,set methods 
+// The block of “set” and “get” is known as Accessors. It is very essential to restrict the accessibility of the property. There are two types of accessors: get accessors and set accessors.
+
+// class Program
+// {
+//     private int age;
+//     public int Get_Age
+//     {
+//         get{return age;}
+//     }
+//     public int Set_Age
+//     {
+//         set
+//         {
+//             if(value>=0){age=value;}
+//             else
+//             {
+//                 Console.WriteLine("Age cannot be negtive");
+//             }
+//         }
+//     }
+// }
+
+// class C
+// {
+//     public static void Main()
+//     {
+//         Program p=new Program();
+//         p.Set_Age=-25;
+//         Console.WriteLine(p.Get_Age);
+//     }
+// }
+
+//auto implemented get set
+// class Prog
+// {
+//     public string name
+//     {
+//         get;
+//         set;
+//     }="Krrish";
+// }
+// class M
+// {
+//     public static void Main()
+//     {
+//         Prog p=new Prog();
+//         Console.WriteLine(p.name);
+//         p.name="Mittal";
+//         Console.WriteLine(p.name);
+//     }
+// }
+
+
+//Constructors in C# these are automatically called when the object of the class is being created it has the same name as the class and it doesnot have any return type and used to initialize the class's object data members
+// class P
+// {
+//     public P()
+//     {
+//         Console.WriteLine("Constructor Called");
+//     }
+// }
+// class M
+// {
+//     public static void Main()
+//     {
+//         P obj=new P();
+//     }
+// }
+
+// Constructor of 5 types
+// default with no parameters
+
+// parameterized with parameters at least one
+// class Program
+// {
+//     int n;
+//     Program(int n)
+//     {
+//         this.n=n;
+//         Console.WriteLine(n);
+//     }
+//     public static void Main()
+//     {
+//         Program p=new Program(3);
+//     }
+// }
+
+// copy creating a new object by copying the values from an existing object of the same class
+// class Program
+// {
+//     string month;
+//     int year;
+//     Program (Program obj)
+//     {
+//         month=obj.month;
+//         year=obj.year;
+//     }
+//     Program(string month,int year){
+//         this.month=month;
+//         this.year=year;
+//     }
+//     public  string details{
+//         get{
+//             return "month: "+month +" year: "+year.ToString();
+//         }
+//     } 
+//     public static void Main()
+//     {
+//         Program p1=new Program("January",2004);
+//         Program p2=new Program(p1);
+//         Console.WriteLine(p2.details);
+//     }
+// }
+
+// static 
+
+
+// private Using a private constructor prevents the creation of the instances of that class it may cause error when we try to create the object of the class we use this when we only have static members in our class
+// class Prog
+// {
+//     private Prog()
+//     {
+//         Console.WriteLine("Private constructor");
+//     }
+//     public static int cnt;
+//     public static int inc()
+//     {
+//         return cnt++;
+//     }
+//     public static void Main()
+//     {
+//         Prog.cnt=10;
+//         Console.WriteLine(Prog.cnt);
+//         Prog.inc();
+//         Console.WriteLine(Prog.cnt);
+//     }
+// }
+
